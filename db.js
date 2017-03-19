9143dd6aaf970394afa849e21ee57f396ee9ad5a
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-03-19 08:29:02 -0400

    Update db.js

--- db.js
+++ db.js
@@ -9,3 +9,35 @@ const Conn = new Sequelize(
     host: 'localhost'
   }
 );
+
+const Person = Conn.define('person', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+const Post = Conn.define('post', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+// Relations
+Person.hasMany(Post);
+Post.belongsTo(Person);
